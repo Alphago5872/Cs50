12da2a2bcebb6268bd8371f6942dc2507c396ab2
Alphago5872 <alpha.kittikhom@gmail.com>
2022-11-08 12:40:21 +0700

    Style change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,32 +4,31 @@ const Homepage = () => {
   return (
     <div class="homepage">
       <h1>Hi, Welcome to my mind</h1>
-      <div>
-        <img
-          src="https://media.tenor.com/mugZdxy-l5EAAAAM/welcome-gif.gif"
-          alt="Hello gif"
-        />
-      </div>
+
+      <img
+        src="https://media.tenor.com/mugZdxy-l5EAAAAM/welcome-gif.gif"
+        alt="Hello gif"
+      />
+
       <h1>
         This website will be giving you a tour of my mind and at the end, there
         will be a quiz
       </h1>
-      <div>
-        <img
-          src="https://media2.giphy.com/media/LOznMvZUKneOhiIscg/200w.gif?cid=6c09b952pti7ovoko6ksyfwvyfeqn5i4p72o37poipgq1eyb&rid=200w.gif&ct=g"
-          alt="Quiz gif"
-        />
-      </div>
+
+      <img
+        src="https://media2.giphy.com/media/LOznMvZUKneOhiIscg/200w.gif?cid=6c09b952pti7ovoko6ksyfwvyfeqn5i4p72o37poipgq1eyb&rid=200w.gif&ct=g"
+        alt="Quiz gif"
+      />
+
       <h1>
         Anyone who finishes the quiz will be rewarded with a fine one cubic feet
         of O2 gas
       </h1>
-      <div>
-        <img
-          src="https://thumbs.gfycat.com/GloriousAgreeableHuman-size_restricted.gif"
-          alt="Reward gif"
-        />
-      </div>
+
+      <img
+        src="https://thumbs.gfycat.com/GloriousAgreeableHuman-size_restricted.gif"
+        alt="Reward gif"
+      />
     </div>
   );
 };
